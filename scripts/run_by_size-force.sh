a03bcfc07f445b122c90b875f552dc98078949e1
Mark Redar <mredar@users.noreply.github.com>
2010-12-10 10:47:43 -0800

    remove debug code

--- scripts/run_by_size-force.sh
+++ scripts/run_by_size-force.sh
@@ -34,7 +34,6 @@ else
     echo "DATA DIR: ${DATA_DIR}"
 fi
 
-exit;
 nice -19 ${PATH_TO_PROGS}/pdf_gen_by_size_parallel.sh -outdir=parallel -logprefix=${LOG_DIR}/pdf_gen_by_size_parallel -cssfile=${PATH_TO_PROGS}/oac_pdf.css -force ${DATA_DIR} 2>&1 > ${LOG_DIR}/pdf_gen_by_size_parallel.out
 
 set logsnip = '`tail ${LOG_DIR}/pdf_gen_by_size_parallel.log`'
