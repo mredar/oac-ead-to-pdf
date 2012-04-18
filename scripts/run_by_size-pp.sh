94669dbbbb0a5a753c572406a1874c44c44da511
Mark Redar <mredar@users.noreply.github.com>
2012-04-18 09:21:57 -0700

    change for mailx style email

---
+++ scripts/run_by_size-pp.sh
@@ -0,0 +1,51 @@
+#! /bin/sh
+
+SCRIPT=`readlink -f $0`
+SCRIPTS_DIR=`dirname ${SCRIPT}`
+
+. ${SCRIPTS_DIR}/set-pdf-env.sh
+
+if [ $PATH_TO_PROGS ]
+then
+    echo "PATH to Programs: ${PATH_TO_PROGS}"
+else
+    PATH_TO_PROGS=`dirname ${SCRIPTS_DIR}`
+    echo "PATH to Programs: ${PATH_TO_PROGS}"
+fi
+if [ $CLASSPATH ]
+then
+    echo "CLASSPATH: ${CLASSPATH}"
+else
+    CLASSPATH=$PATH_TO_PROGS/javalib/lib/saxonb-8.9.jar:$PATH_TO_PROGS/javalib/classes:${CLASSPATH}
+    echo "CLASSPATH: ${CLASSPATH}"
+fi
+if [ $LOG_DIR ]
+then
+    echo "LOG DIR: ${LOG_DIR}"
+else
+    LOG_DIR=${PATH_TO_PROGS}/log
+    echo "LOG DIR: ${LOG_DIR}"
+fi
+if [  $DATA_DIR ]
+then
+    echo "DATA DIR: ${DATA_DIR}"
+else
+    DATA_DIR=/dsc/data/in/oac-ead/prime2002/
+    echo "DATA DIR: ${DATA_DIR}"
+fi
+
+nice -9 ${PATH_TO_PROGS}/pdf_gen_by_size_parallel.sh -outdir=parallel -logprefix=${LOG_DIR}/pdf_gen_by_size_parallel -cssfile=${PATH_TO_PROGS}/oac_pdf.css -exclude_file=${SCRIPTS_DIR}/exclude.list ${DATA_DIR} 2>&1 > ${LOG_DIR}/pdf_gen_by_size_parallel.out
+
+if [ ${PRODUCTION} = "true" ]; then
+#datefix=`date +%Y-%m-%d`
+#logsnip=`tail ${LOG_DIR}/pdf_gen_by_size_parallel-${datefix}.log`
+logsnip=`tail ${LOG_DIR}/pdf_gen_by_size_parallel.out`
+
+mail -s "PDF Job Done" mark.redar@ucop.edu <<%%
+pdf_gen_by_size_parallel prime2002 for dsc on ${VORO_HOSTNAME} run finished.
+
+$logsnip
+
+
+%%
+fi
---
+++ scripts/run_parallel_path_from_file.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+SCRIPT=`readlink -f $0`
+SCRIPTS_DIR=`dirname ${SCRIPT}`
+
+. ${SCRIPTS_DIR}/set-pdf-env.sh
+
+if [ $PATH_TO_PROGS ]
+then
+    echo "PATH to Programs: ${PATH_TO_PROGS}"
+else
+    PATH_TO_PROGS=`dirname ${SCRIPTS_DIR}`
+    echo "PATH to Programs: ${PATH_TO_PROGS}"
+fi
+if [ $CLASSPATH ]
+then
+    echo "CLASSPATH: ${CLASSPATH}"
+else
+    CLASSPATH=$PATH_TO_PROGS/javalib/lib/saxonb-8.9.jar:$PATH_TO_PROGS/javalib/classes:${CLASSPATH}
+    echo "CLASSPATH: ${CLASSPATH}"
+fi
+if [  $DATA_DIR ]
+then
+    echo "DATA DIR: ${DATA_DIR}"
+else
+    DATA_DIR=/dsc/data/in/oac-ead/prime2002/
+    echo "DATA DIR: ${DATA_DIR}"
+fi
+
+
+nice -9 python ${PATH_TO_PROGS}/pdf_gen.py --savehtml --force --css=${PATH_TO_PROGS}/oac_pdf.css --outdir=parallel --data_root=${DATA_DIR} --file=$1 2>&1 > run_file.out
